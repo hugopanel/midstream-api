312b6742f8f1122699239b6dd112166fb224d439
ArthB94 <80051198+ArthB94@users.noreply.github.com>
2024-05-29 19:18:04 +0200

    déclarations des premières classes

--- Domain/Entities/File.cs
+++ Domain/Entities/File.cs
@@ -1,10 +1,11 @@
 namespace Domain.Entities
 {
-    public class Project
+    public class File
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string Path { get; set; }
         public string Description { get; set; }
-        public DateTime Beginning_date { get; set; }
+        public string Extension { get; set; }
     }
 }
