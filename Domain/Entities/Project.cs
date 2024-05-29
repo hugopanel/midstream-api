1b914ce5d82fea6013808747bfaff238fa032433
ArthB94 <80051198+ArthB94@users.noreply.github.com>
2024-05-29 18:55:32 +0200

    init

---
+++ Domain/Entities/Project.cs
@@ -0,0 +1,17 @@
+namespace Domain.Entities
+{
+    public class Project
+    {
+        public Guid Id { get; set; }
+        {
+	"id": "",
+	"name": "",
+	"description": "",
+	"beginning_date": ""
+}
+```
+        public string name { get; set; }
+        public string PasswordHash { get; set; }
+        public string Email { get; set; }
+    }
+}
