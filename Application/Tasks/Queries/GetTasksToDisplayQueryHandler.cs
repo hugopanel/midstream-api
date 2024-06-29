04aff666453b8b8a8b0c088d7860919a7da06ad4
paulinedavid <pauline.david@efrei.net>
2024-06-29 18:01:46 +0200

    page tasks finie

--- Application/Tasks/Queries/GetTasksToDisplayQueryHandler.cs
+++ Application/Tasks/Queries/GetTasksToDisplayQueryHandler.cs
@@ -1,40 +1,46 @@
 using Application.Common.Interfaces;
 using Application.Common.Interfaces.Authentication;
 using Application.Common.Interfaces.Persistence;
+using Application.Tasks;
 using Domain.Entities;
 using MediatR;
 
 namespace Application.Tasks.Queries;
 
-public class GetTasksToDisplayToDisplayQueryHandler(ITaskRepository taskRepository, IJwtTokenGenerator jwtTokenGenerator)
+public class GetTasksToDisplayToDisplayQueryHandler(ITaskRepository taskRepository, IUserRepository userRepository, IJwtTokenGenerator jwtTokenGenerator)
     : IRequestHandler<GetTasksToDisplayQuery, ListTasksToDisplayResult>
 {
     private ITaskRepository _taskRepository = taskRepository;
+    private IUserRepository _userRepository = userRepository;
     private IJwtTokenGenerator _jwtTokenGenerator = jwtTokenGenerator;
 
     public async Task<ListTasksToDisplayResult> Handle(GetTasksToDisplayQuery query, CancellationToken cancellationToken)
     {
         var tasks = _taskRepository.GetTachesByProject(query.projectId);
 
+        var tasksToDisplay = new List<TaskToDisplay>();
         var types = new List<string>();
-
-        foreach (var task in tasks)
-        {
-            if(task.TypeOfTask != null && !types.Contains(task.TypeOfTask)){
-                types.Add(task.TypeOfTask);
-            }
-        }
-
         var priorities = new List<string>();
 
         foreach (var task in tasks)
         {
-            if(task.Priority != null && !priorities.Contains(task.Priority)){
+            var user = _userRepository.GetUserById(task.AssignedTo);
+
+            var taskToDisplay = new TaskToDisplay(task.Id, task.BeginningDate, task.EndDate, task.Priority, task.Status, task.TypeOfTask, task.Title, task.Description, task.Belong, task.Author, task.AssignedTo, task.RelatedTo, user.Avatar, user.Colour);
+            tasksToDisplay.Add(taskToDisplay);
+
+            if (task.TypeOfTask != null && !types.Contains(task.TypeOfTask))
+            {
+                types.Add(task.TypeOfTask);
+            }
+
+            if (task.Priority != null && !priorities.Contains(task.Priority))
+            {
                 priorities.Add(task.Priority);
             }
         }
 
         // Return all roles
-        return new ListTasksToDisplayResult(tasks, types, priorities);
+        return new ListTasksToDisplayResult(tasksToDisplay, types, priorities);
     }
 }
